c8b0118880947d9d127594a5f86839bda5c02ae9
siddhesh_patwa <127028705+siddheshpatwa@users.noreply.github.com>
2026-06-21 20:15:01 +0530

    one bug fixed added in bull mq .

--- leaderboard-api/src/queue/score.queue.ts
+++ leaderboard-api/src/queue/score.queue.ts
@@ -1,15 +1,31 @@
-import "../config/env"; // must be first — loads .env before any other imports
+// import "../config/env"; // must be first — loads .env before any other imports
+// import { Queue } from "bullmq";
+
+// export const scoreQueue = new Queue("scoreQueue",{
+//  connection:{
+//    host: process.env.REDIS_HOST,
+//    port:Number(process.env.REDIS_PORT)
+//  }
+// });
+
+
+// // await scoreQueue.add("update-score",{
+// //  email,
+// //  score
+// // });
+import "../config/env";
 import { Queue } from "bullmq";
 
-export const scoreQueue = new Queue("scoreQueue",{
- connection:{
-   host: process.env.REDIS_HOST,
-   port:Number(process.env.REDIS_PORT)
- }
-});
+const redisUrl = new URL(
+  process.env.REDIS_URL!
+);
 
-
-// await scoreQueue.add("update-score",{
-//  email,
-//  score
-// });
+export const scoreQueue = new Queue(
+  "scoreQueue",
+  {
+    connection: {
+      host: redisUrl.hostname,
+      port: Number(redisUrl.port),
+    },
+  }
+);
